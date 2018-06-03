a8961f0c34d1db626a6b317eee304486705b8ba3
HugoHBSPinto <HugoPintoPortC@gmail.com>
2018-06-03 18:17:16 +0100

    Add JogadorUserControlView and JogadorUserControlViewModel.
    Add JogadorUserControlView in folder Views.
    Add JogadorUserControlViewModel in folder ViewModels.
    
    Relation between the two not yet implemented.

--- IT4ClubCar/IT4ClubCar/IT4ClubCar/Views/JogadorUserControlView.xaml.cs
+++ IT4ClubCar/IT4ClubCar/IT4ClubCar/Views/JogadorUserControlView.xaml.cs
@@ -10,9 +10,9 @@ using Xamarin.Forms.Xaml;
 namespace IT4ClubCar.IT4ClubCar.Views
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class JogadorView : Grid
+	public partial class JogadorUserControlView : Grid
 	{
-		public JogadorView ()
+		public JogadorUserControlView ()
 		{
 			InitializeComponent ();
 		}

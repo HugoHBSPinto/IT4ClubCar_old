68ae45e6688a57c3a41eac592b37c36b3c2b1435
HugoHBSPinto <HugoPintoPortC@gmail.com>
2018-06-03 09:55:47 +0100

    Add the PaginaInicialViewModel
    Add in the ViewModels folder a ViewModel for the PaginaInicialView.
    Add a command that will take the app to the GameConfigurationView (not yet created).
    
    Warning : The relation between the view and the viewmodel isn't yet done.

---
+++ IT4ClubCar/IT4ClubCar/IT4ClubCar/ViewModels/PaginaInicialViewModel.cs
@@ -0,0 +1,40 @@
+﻿using IT4ClubCar.IT4ClubCar.Interfaces;
+using IT4ClubCar.IT4ClubCar.Views;
+using IT4ClubCar_IT4ClubCar_ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace IT4ClubCar.IT4ClubCar.ViewModels
+{
+    class PaginaInicialViewModel : BaseViewModel
+    {
+        /// <summary>
+        /// Executado quando o utilizador clica no botão "Começar Jogo."
+        /// Executa o método IrParaPaginaConfigurarJogo do IT4ClubCarNavgationService.
+        /// </summary>
+        private ICommand _irParaPaginaConfigurarJogo;
+        public ICommand IrParaPaginaComConfigurarJogo
+        {
+            get
+            {
+                if (_irParaPaginaConfigurarJogo == null)
+                    _irParaPaginaConfigurarJogo = new Command(p => base.NavigationService.NavegarParaPaginaConfigurarJogo(),p => { return true; });
+                return _irParaPaginaConfigurarJogo;
+            }
+        }
+
+
+
+        public PaginaInicialViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            
+        }
+
+
+
+    }
+}

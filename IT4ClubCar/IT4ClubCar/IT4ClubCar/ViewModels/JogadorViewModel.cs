bc456332f239daf117d58165a1a807aa7dab4bbe
HugoHBSPinto <HugoPintoPortC@gmail.com>
2018-06-04 21:01:31 +0100

    Add a JogadorViewModel
    Add in the ViewModels folder a ViewModel to wrap the JogadorModel.

---
+++ IT4ClubCar/IT4ClubCar/IT4ClubCar/ViewModels/JogadorViewModel.cs
@@ -0,0 +1,76 @@
+﻿using IT4ClubCar.IT4ClubCar.Models;
+using IT4ClubCar_IT4ClubCar_ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IT4ClubCar.IT4ClubCar.ViewModels
+{
+    class JogadorViewModel : BaseViewModel
+    {
+        private JogadorModel _jogadorModel;
+
+        /// <summary>
+        /// Obtém id do jogador.
+        /// </summary>
+        public int Id
+        {
+            get
+            {
+                return _jogadorModel.Id;
+            }
+        }
+        
+        /// <summary>
+        /// Obtém ou define o nome do jogador.
+        /// </summary>
+        public string Nome
+        {
+            get
+            {
+                return _jogadorModel.Nome;
+            }
+            set
+            {
+                _jogadorModel.Nome = value;
+                OnPropertyChanged("Id");
+            }
+        }
+
+        /// <summary>
+        /// Obtém ou define a idade do jogador.
+        /// </summary>
+        public int Idade
+        {
+            get
+            {
+                return _jogadorModel.Idade;
+            }
+            set
+            {
+                _jogadorModel.Idade = value;
+                OnPropertyChanged("Idade");
+            }
+        }
+
+        /// <summary>
+        /// Obtém o valor do jogador.
+        /// </summary>
+        public bool Ativo
+        {
+            get
+            {
+                return _jogadorModel.Ativo;
+            }
+        }
+
+
+        public JogadorViewModel(JogadorModel jogadorModel) : base()
+        {
+            _jogadorModel = jogadorModel;
+        }
+
+
+
+    }
+}

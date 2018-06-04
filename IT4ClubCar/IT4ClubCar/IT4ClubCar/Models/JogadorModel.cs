0c10a7539a82ddff9c513697465fa6e34a8faf58
HugoHBSPinto <HugoPintoPortC@gmail.com>
2018-06-04 21:00:46 +0100

    Add JogadorModel
    Add to the models folder a JogadorModel with SQlLite references to create a table in the local database.

---
+++ IT4ClubCar/IT4ClubCar/IT4ClubCar/Models/JogadorModel.cs
@@ -0,0 +1,56 @@
+﻿using IT4ClubCar_IT4ClubCar_Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IT4ClubCar.IT4ClubCar.Models
+{
+    [Table("Jogador")]
+    class JogadorModel : BaseModel
+    {
+        /// <summary>
+        /// Obtém ou define o id.
+        /// </summary>
+        [PrimaryKey, Column("Id")]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Obtém ou define o nome.
+        /// </summary>
+        [Column("Nome")]
+        public string Nome { get; set; }
+
+        /// <summary>
+        /// Obtém ou define a idade.
+        /// </summary>
+        [Column("Idade")]
+        public int Idade { get; set; }
+
+        /// <summary>
+        /// Obtém ou define se o jogador está ativo.
+        /// </summary>
+        [Column("Ativo")]
+        public bool Ativo { get; set; }
+
+
+
+        public JogadorModel()
+        {
+
+        }
+
+
+
+        public JogadorModel(int id,string nome, int idade, bool ativo)
+        {
+            Id = id;
+            Nome = nome;
+            Idade = idade;
+            Ativo = ativo;
+        }
+
+
+
+    }
+}
